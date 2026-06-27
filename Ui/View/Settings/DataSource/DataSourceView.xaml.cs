2885e431d5eefb4d9a32365478c6fb5c8ce845b6
gansui <mr.tliang@gmail.com>
2026-06-27 13:30:44 +0800

    fix: 创建缺失的DataSourceView.xaml.cs代码隐藏文件

---
+++ Ui/View/Settings/DataSource/DataSourceView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace _1RM.View.Settings.DataSource
+{
+    public partial class DataSourceView : UserControl
+    {
+        public DataSourceView()
+        {
+            InitializeComponent();
+        }
+    }
+}
